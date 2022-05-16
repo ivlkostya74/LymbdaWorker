cc0d5eed2d593382e1b650af605449187e613919
ivlkostya74 <ivlkostya@yandex.ru>
2022-05-17 08:17:57 +1000

    correct one

--- src/Homwork/Worker.java
+++ src/Homwork/Worker.java
@@ -4,26 +4,27 @@ public class Worker implements OnTaskDoneListener, OnTaskErrorListener {
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskErrorListener errorCallback) {
+    public Worker(OnTaskDoneListener callback,OnTaskErrorListener errorCallback) {
         this.errorCallback = errorCallback;
-    }
-
-    public Worker(OnTaskDoneListener callback) {
         this.callback = callback;
     }
 
+
+
+//    public Worker() {
+//
+//    }
+
     public void start() {
         for (int i = 0; i < 100; i++) {
-            try {
+
                 if (i != 33) {
 
                     callback.onDone("Task " + i + " is done");
                 } else {
                     errorCallback.onError("Task " + i + " is not completed ");
                 }
-            } catch (NullPointerException e) {
-                System.out.println(e.getMessage());
-            }
+
         }
     }
 
